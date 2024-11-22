4b3268b13e1458567860d62c70dce3541383b91c
jethakaur <j.kaur0720211@arts.ac.uk>
2024-11-22 13:49:34 +0000

    files

---
+++ script.js
@@ -0,0 +1,16 @@
+// Before DOM loads show this animation
+// https://dev.to/lensco825/how-to-quickly-add-a-loading-screen-onto-your-website-7ga
+
+// Message Function
+// Wait for DOM to load
+// code for location data
+// https://www.w3schools.com/html/html5_geolocation.asp#:~:text=The%20HTML%20Geolocation%20API%20is%20used%20to%20get%20the%20geographical,unless%20the%20user%20approves%20it.&text=Note%3A%20Geolocation%20is%20most%20accurate,devices%20with%20GPS%2C%20like%20smartphones.
+
+// If location is approved
+// get submitted message and get position from location API 
+// Check collected messages and compare them to user location
+// if the difference between user and message location is small position message closer to the center 
+// if user and message location is equal check time data if the difference is small position message closer to the center 
+
+// Centralise User
+// if clicked move html doc bac
